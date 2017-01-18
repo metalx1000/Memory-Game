b69873d5883cec8a6afc26b2305a69964bc3bc94
metalx1000 <git@filmsbykris.com>
2017-01-17 19:36:38 -0500

    started main script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var game = new Phaser.Game(640, 540, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(768, 540, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var cards=[];
+var cardsv=[];
 
 function preload() {
 
@@ -8,14 +10,39 @@ function preload() {
 }
 
 function create() {
-
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    for(i=0;i<5;i++){
-      for(z=0;z<3;z++){
-        game.add.sprite(128*i, 180*z, 'cardback');
-      }
-    }
+  loadCards();
+      var card = game.add.sprite(300, 300, 'cardback');
+      card.inputEnable = true;
+      card.events.onInputDown.add(function(){
+        console.log("test");
+      });
 
 }
 
+
+function loadCards(){
+  //creat values for cards
+  for(var i = 0;i<18;i++){
+    cardsv.push(i);
+  }
+
+  var x=0;
+  for(var i=0;i<6;i++){
+    for(var z=0;z<3;z++){
+      var card = game.add.sprite(128*i, 180*z, 'cardback');
+      card.inputEnable = true;
+      card.events.onInputDown.add(function(){
+        console.log("test");
+      });
+      //console.log(cardsv[x]);
+      card.value=cardsv[x];
+      x++;
+      cards.push(card);
+    }
+  }
+}
+
+function cardClicked(){
+  console.log(this);
+  console.log("test");
+}

1d9b0f84ae457ccb17457f5b7b401db7613438f4
metalx1000 <git@filmsbykris.com>
2017-01-23 12:23:37 -0500

    basic card grid with shuffled values

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var game = new Phaser.Game(768, 540, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
-var cards=[];
-var cardsv=[];
+var cards;
+var cardsv = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+//shuffle card values
+shuffle(cardsv);
 
 function preload() {
 
@@ -10,39 +12,33 @@ function preload() {
 }
 
 function create() {
-  loadCards();
-      var card = game.add.sprite(300, 300, 'cardback');
-      card.inputEnable = true;
-      card.events.onInputDown.add(function(){
-        console.log("test");
-      });
-
+  createDeck();
 }
 
-
-function loadCards(){
-  //creat values for cards
-  for(var i = 0;i<18;i++){
-    cardsv.push(i);
-  }
-
-  var x=0;
-  for(var i=0;i<6;i++){
-    for(var z=0;z<3;z++){
-      var card = game.add.sprite(128*i, 180*z, 'cardback');
-      card.inputEnable = true;
-      card.events.onInputDown.add(function(){
-        console.log("test");
-      });
-      //console.log(cardsv[x]);
-      card.value=cardsv[x];
-      x++;
-      cards.push(card);
-    }
-  }
+function cardClicked(card){
+  console.log(card.value);
 }
 
-function cardClicked(){
-  console.log(this);
-  console.log("test");
+function createDeck(){
+  var i = 0;
+  cards = game.add.group();
+  cards.createMultiple(18,"cardback",[0],true);
+  cards.align(6,-1,128,180);
+  cards.forEach(function(c){
+    c.inputEnabled = true;
+    c.events.onInputDown.add(cardClicked,this);
+    c.value = cardsv[i];
+    i++;
+  });
+}
+
+//shuffle array
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
 }
